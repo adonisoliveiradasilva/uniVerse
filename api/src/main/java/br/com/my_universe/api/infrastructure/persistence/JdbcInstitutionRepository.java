cb53dbf6443c099982c1a871459fa1bc1ec3197d
adonis oliveira <adonis.silva@grupolpj.com>
2025-10-23 14:15:01 -0300

    feat(ui e api): adiciona getAllInstitutions

--- api/src/main/java/br/com/my_universe/api/infrastructure/persistence/JdbcInstitutionRepository.java
+++ api/src/main/java/br/com/my_universe/api/infrastructure/persistence/JdbcInstitutionRepository.java
@@ -1,15 +1,12 @@
 package br.com.my_universe.api.infrastructure.persistence;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import br.com.my_universe.api.application.ports.InstitutionRepository;
 import br.com.my_universe.api.domain.Institution;
 
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
